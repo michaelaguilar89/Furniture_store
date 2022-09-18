7d2b3b0f8c7d57ebdb8a90d13a6fd61c7425cdaf
Michael_Aguilar <mchlaguilar051@hotmail.es>
2022-09-17 21:47:59 -0600

    creando componentes header,products y footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { ProductsComponent } from './products/products.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProductsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
